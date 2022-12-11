7fe17908cbe901cdf0552fe3659c74131f28f926
manishsimplegithub <manishsrivastav@bbdu.ac.in>
2022-12-12 00:35:54 +0530

    commit

---
+++ build/precache-manifest.8e5d7e0247e15a76b490f824155a16db.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdd1067005c7efe2bdb1",
+    "url": "/live-website/static/css/main.39f149dd.chunk.css"
+  },
+  {
+    "revision": "cdd1067005c7efe2bdb1",
+    "url": "/live-website/static/js/main.c55f80a4.chunk.js"
+  },
+  {
+    "revision": "8d643eddeb390df74bf9",
+    "url": "/live-website/static/js/runtime~main.06ec1254.js"
+  },
+  {
+    "revision": "1329560497956d6618b2",
+    "url": "/live-website/static/css/2.26416672.chunk.css"
+  },
+  {
+    "revision": "1329560497956d6618b2",
+    "url": "/live-website/static/js/2.37206b72.chunk.js"
+  },
+  {
+    "revision": "2c6b046a7682591c78d0",
+    "url": "/live-website/static/js/3.0dd53fc1.chunk.js"
+  },
+  {
+    "revision": "a4e5e1d120a225c88d3acbb2b427988c",
+    "url": "/live-website/static/media/web.a4e5e1d1.jpg"
+  },
+  {
+    "revision": "14a0ce4298a15a8b665653b409746067",
+    "url": "/live-website/static/media/OIP.14a0ce42.jpeg"
+  },
+  {
+    "revision": "334859d2207b2bf0e15f34d8c206af0f",
+    "url": "/live-website/static/media/second.334859d2.webp"
+  },
+  {
+    "revision": "c3946173adc86e5f28193b6ce01b809e",
+    "url": "/live-website/static/media/app.c3946173.jpg"
+  },
+  {
+    "revision": "ae00bb043255ad651a9d52356dcf3fcc",
+    "url": "/live-website/static/media/software.ae00bb04.jpg"
+  },
+  {
+    "revision": "3942283e9e0cf8031f2a3ac821c57f06",
+    "url": "/live-website/static/media/digital.3942283e.jpg"
+  },
+  {
+    "revision": "a516313fc18d5225b5334058feb243c9",
+    "url": "/live-website/static/media/flutter.a516313f.jpg"
+  },
+  {
+    "revision": "3466b0e9f41acbf72f40b56196821e27",
+    "url": "/live-website/static/media/java.3466b0e9.jpg"
+  },
+  {
+    "revision": "53098b9d41ebb6de32150605a773e087",
+    "url": "/live-website/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/live-website/precache-manifest.8e5d7e0247e15a76b490f824155a16db.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/live-website/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
